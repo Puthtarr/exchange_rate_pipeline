8f23226f6a4d816db3d860e9dfc9d6ed64883aad
Puthtarr <piyaphatputt01@gmail.com>
2025-06-17 02:03:41 +0700

    revise for run in airflow

--- src/ingest/fetch_exchange_rate.py
+++ src/ingest/fetch_exchange_rate.py
@@ -4,9 +4,25 @@ from dotenv import load_dotenv
 from datetime import datetime, timedelta
 import pandas as pd
 import json
-from src.settings import data_dir, log_dir, raw_data, date
+# from src.settings import data_dir, log_dir, raw_data, date
 import logging
 load_dotenv()
+
+project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+
+data_dir = os.path.join(project_dir, "data")
+raw_data = os.path.join(data_dir, 'raw')
+clean_data = os.path.join(data_dir, 'clean')
+config_dir = os.path.join(project_dir, "config")
+log_dir = os.path.join(project_dir, 'logging')
+os.makedirs(data_dir, exist_ok=True)
+os.makedirs(config_dir, exist_ok=True)
+os.makedirs(log_dir, exist_ok=True)
+os.makedirs(raw_data, exist_ok=True)
+os.makedirs(clean_data, exist_ok=True)
+
+date = datetime.now().strftime('%Y-%m-%d')
+
 
 logging.basicConfig(
     filename=os.path.join(log_dir, 'fetch_exchange_rate.log'),
